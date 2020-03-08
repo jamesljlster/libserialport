a40a8e80b43ee04a6489600cad85ab175ee27ffb
Zheng-Ling Lai <jamesljlster@gmail.com>
2020-03-09 02:58:17 +0800

    Fix missing definitions which set to zero

--- cmake_config_gen.py
+++ cmake_config_gen.py
@@ -1,5 +1,22 @@
 from os.path import exists
 
+# Let definitions exists in config.h even its value is set to zero
+persistent = ['PACKAGE_BUGREPORT',
+              'PACKAGE_NAME',
+              'PACKAGE_STRING',
+              'PACKAGE_TARNAME',
+              'PACKAGE_URL',
+              'PACKAGE_VERSION',
+              'SP_LIB_VERSION_AGE',
+              'SP_LIB_VERSION_CURRENT',
+              'SP_LIB_VERSION_REVISION',
+              'SP_LIB_VERSION_STRING',
+              'SP_PACKAGE_VERSION_MAJOR',
+              'SP_PACKAGE_VERSION_MICRO',
+              'SP_PACKAGE_VERSION_MINOR',
+              'SP_PACKAGE_VERSION_STRING'
+              ]
+
 if not exists('config.h.in'):
     print('config.h.in does not exist!')
     exit()
@@ -14,5 +31,9 @@ for line in src.readlines():
     comp = line.split(' ')
     if comp[0] == '#undef':
         token = comp[1].strip('\n')
-        line = '#cmakedefine {} @{}@\n'.format(token, token)
+        if token in persistent:
+            prefix = '#define'
+        else:
+            prefix = '#cmakedefine'
+        line = '{} {} @{}@\n'.format(prefix, token, token)
     config.write(line)
